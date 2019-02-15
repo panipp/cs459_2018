61fac3c4cabf61a446b1642df9b848cfb312a74d
SothanaV <eimpix.ice@gmail.com>
2019-02-15 15:00:16 +0700

    query & export excel

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'django_extensions',
+    'import_export',
 ]
+IMPORT_EXPORT_USE_TRANSACTIONS = True
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
